8b5d9e7be1f32508aedabc30e83a267ec53f38d0
dilendrauikey8 <dilendrauikey82178@gmail.com>
2022-09-08 01:50:01 +0530

    added extension

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# speed_skip_ext
+# speed-ads-ext
+# About
+This is Firefox extension that can control the speed of videos through keyboard on any  site.
+Also able to skip ads on youtube.
+
+# How To Use
+
+Download  This File [video_speed_and_skip_ads-1.3-fx.xpi](https://github.com/bhuvan-byte/speed-ads-ext/raw/master/video_speed_and_skip_ads-1.3-fx.xpi) and Then open it using firefox.\
+Now stream any video straming website and use the following keys. \
+Key 'Z': Decrease video speed by 0.25 (press shift key to change speed by 0.5)\
+Key 'X': Increase video speed by 0.25 (press shift key to change speed by 0.5)\
+Key 'S': Skip youtube ads <br/>
---
+++ index.js
@@ -0,0 +1,23 @@
+function changeSpeed(videoList, delta){
+    for(const video of videoList) video.playbackRate +=  delta;
+}    
+
+    document.addEventListener('keydown', function(event) {
+        var vlist = document.querySelectorAll('video');
+        if (event.code == 'KeyX') {
+            changeSpeed(vlist,0.25);
+        } else if (event.code == 'KeyZ') {
+            changeSpeed(vlist,-0.25);
+        } else if (event.code == 'KeyX' && (event.shiftKey || event.metaKey)) {
+            changeSpeed(vlist,0.5);
+        } else if (event.code == 'KeyZ' && (event.shiftKey || event.metaKey)) {
+            changeSpeed(vlist,-0.5);
+        } else if (event.code == 'KeyS') {
+            for(const vobj of vlist)  vobj.currentTime = (vobj.duration);
+
+	        for (const button of document.getElementsByClassName("ytp-ad-skip-button")) {
+			    button.click(); // "Skip Ad" or "Skip Ads" buttons
+			}
+            //jfpn iwewne skldfndnf
+        }
+    });
